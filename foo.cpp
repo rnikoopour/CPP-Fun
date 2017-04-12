99497e928085d14a4e13a7f90354d7d7c56c534c
Reza Nikoopour <rezanikoopour@foobar.local>
2017-04-11 18:26:59 -0700

    Function Composition

---
+++ foo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template <class Func> struct WrappedFunc {
+  Func function;
+};
+
+template <class T>
+struct wrapped {
+  T val;
+};
+
+template <class T, class T2> 
+auto operator|(wrapped<T> lhs, T2 rhs) {
+  return rhs(lhs);
+}
+
+template <class T> 
+auto add(const T& rhs) {
+  return [=](auto lhs) { return wrapped<T>{lhs.val + rhs}; };
+}
+
+auto add_bar = add<std::string>(" bar");
+
+int main() {
+  const auto foo = (wrapped<std::string>{"foo"} | add_bar | add_bar | add_bar).val;
+  std::cout << foo << std::endl;
+}
